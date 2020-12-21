43b70e0278f15e040b5aae7ab134d36d5fcc9878
Altamir Cassiano <altamirdiascassiano@hotmail.com>
2020-12-21 20:30:12 -0300

    First commit

---
+++ SimpleSingletonPatternInCSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleSingletonPatternInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciado");
+
+            //Independente da quantidade de chamada sempre os arquivos terão mesmo inicio pois é o ID da Instancia singleton
+            FileCreator.GetInstance().CreateFile(string.Concat(@"C:\temp\", Guid.NewGuid().ToString()));
+            FileCreator.GetInstance().CreateFile(string.Concat(@"C:\temp\", Guid.NewGuid().ToString()));
+            FileCreator.GetInstance().CreateFile(string.Concat(@"C:\temp\", Guid.NewGuid().ToString()));
+            FileCreator.GetInstance().CreateFile(string.Concat(@"C:\temp\", Guid.NewGuid().ToString()));
+            FileCreator.GetInstance().CreateFile(string.Concat(@"C:\temp\", Guid.NewGuid().ToString()));
+            FileCreator.GetInstance().CreateFile(string.Concat(@"C:\temp\", Guid.NewGuid().ToString()));
+            FileCreator.GetInstance().CreateFile(string.Concat(@"C:\temp\", Guid.NewGuid().ToString()));
+
+            Console.WriteLine("Processamento finalizado");
+            Console.ReadKey();
+        }
+    }
+
+    public sealed class FileCreator
+    {
+        private static FileCreator _instance = null;
+        private static string instanceId = string.Empty;
+        private FileCreator(){}
+        public static FileCreator GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FileCreator();
+                instanceId = Guid.NewGuid().ToString();
+            }
+            return _instance;
+        }        
+        public void CreateFile(string fullPath)
+        {         
+           File.Create(fullPath+"-------"+ instanceId);
+        }
+    }
+}
